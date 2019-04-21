39a762190a6de872e983dfc6afb1e9b92623e1d8
Daniel <amusin@wisc.edu>
2019-04-21 09:41:07 -0500

    Added color options

--- PassThePigs/Pig.cs
+++ PassThePigs/Pig.cs
@@ -6,10 +6,12 @@ namespace PassThePigs
 {
     public class Pig
     {
+        private int color;
         static Random random = new Random();
         public Pig()
         {
-
+            // Temporary default color
+            this.color = 0;
         }
 
         public enum RollOutcomes
@@ -54,6 +56,42 @@ namespace PassThePigs
             {
                 return RollOutcomes.LeaningJowler;
             }
+        }
+
+        /*
+         * Set pig color to int corresponding to color:
+         * 0-Black, 1-White, 2-Pink, 3-Brown, 4-Yellow, 5-Blue, 6-Purple, 7-Green
+         * Return true if successful color change
+         */
+        public boolean ChangeColor(int color) {
+            if (color >= 0 && color <= 7) {
+                this.color = color;
+                return true;
+            }
+            return false;
+        }
+
+        /* 
+         * Fetch name of pig color
+         * 0-Black, 1-White, 2-Pink, 3-Brown, 4-Yellow, 5-Blue, 6-Purple, 7-Green
+         */
+        public String getColor() {
+            if (color = 0)
+                return "Black";
+            else if (color = 1)
+                return "White";
+            else if (color = 2)
+                return "Pink";
+            else if (color = 3)
+                return "Brown";
+            else if (color = 4)
+                return "Yellow";
+            else if (color = 5)
+                return "Blue";
+            else if (color = 6)
+                return "Purple";
+            else
+                return "Green";
         }
     }
 }
